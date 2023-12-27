c118ea8bd84473be566a6186431352f9f7d9d1b2
mfadhili <mfadhili@github.com>
2023-12-27 14:02:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Counter />
     </div>
+  );
+}
+
+function Counter() {
+    const [step, setStep] = useState(1);
+    const [count, setCount] = useState(0);
+    //const [date, setDate] = useState(Date());
+    const date = new Date();
+    console.log(date)
+    date.setDate(date.getDate() + count)
+
+    function handleMinus() {
+        setCount(count - step);
+    }
+
+    function handlePlus() {
+        setCount(count+step);
+    }
+
+    function handleReset() {
+        setCount(0);
+        setStep(1);
+    }
+
+    function handleSlider(e) {
+        setStep(Number(e.target.value))
+    }
+
+    return (
+      <div>
+          <div className="">
+              <input type="range" min={"0"} max={"10"} value={step} onChange={(e) => handleSlider(e)}/>
+              <span>Step: {step}</span>
+          </div>
+
+          <div className="">
+              <button onClick={handleMinus}>-</button>
+              <input type="text" value={count}/>
+              <button onClick={handlePlus}>+</button>
+          </div>
+
+          <p>
+              <span>
+                  {count ===0 ? "Today is " : count === 1 ? `One day from now is ` : count > 1 ? `${count} days from today will be `: `${Math.abs(count)} days ago was ` }
+              </span>
+              <span>{date.toDateString()}</span>
+          </p>
+
+          {
+              count !==0 ?
+                  <div className="">
+                      <button onClick={handleReset}>Reset</button>
+                  </div>
+                  : null
+          }
+
+      </div>
   );
 }
 
